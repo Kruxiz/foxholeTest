2b0388561ee7f65db2b2611451055c836e132e96
Kruxiz <clarke1212@Hotmail.co.uk>
2017-01-25 10:10:37 +0000

    Update and rename SceneManager.h to WorldManager.h

--- FoxholeTest/WorldManager.h
+++ FoxholeTest/WorldManager.h
@@ -9,7 +9,7 @@
 
 #define DEG_TO_RADIAN 0.017453293
 
-namespace SceneManager {
+namespace WorldManager {
 	void init(void);
 	void update(SDL_Window * window, SDL_Event sdlEvent);
 	void draw(SDL_Window *window, float fps);
