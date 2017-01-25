d688dd63009e282b92bb31d6f85bcc1e5e6216cf
Kruxiz <clarke1212@Hotmail.co.uk>
2017-01-25 10:11:49 +0000

    Update WorldManager.h

--- FoxholeTest/WorldManager.h
+++ FoxholeTest/WorldManager.h
@@ -7,8 +7,6 @@
 #include <gtc\matrix_transform.hpp>
 #include <gtc\type_ptr.hpp>
 
-#define DEG_TO_RADIAN 0.017453293
-
 namespace WorldManager {
 	void init(void);
 	void update(SDL_Window * window, SDL_Event sdlEvent);
