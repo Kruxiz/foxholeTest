75a50dcd158706dd78dc36ae758053f97141383b
kath-bruce <kath-bruce@hotmail.com>
2017-01-26 16:03:27 +0000

    it builds and runs now

---
+++ FoxholeGroupProjV1.1/FoxholeGroupProjV1_1/FoxholeGroupProjV1_1/main.cpp
@@ -0,0 +1,84 @@
+#include "rt3d.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+using namespace std;
+
+SDL_Window * setupRC(SDL_GLContext &context) {
+	SDL_Window * window;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) // Initialize video
+		rt3d::exitFatalError("Unable to initialize SDL");
+
+	// Request an OpenGL 3.0 context.
+	// Not able to use SDL to choose profile (yet), should default to core profile on 3.2 or later
+	// If you request a context not supported by your drivers, no OpenGL context will be created
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);  // double buffering on
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4); // Turn on x4 multisampling anti-aliasing (MSAA)
+
+													   // Create 800x600 window
+	window = SDL_CreateWindow("SDL/GLM/OpenGL Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+	if (!window) // Check window was created OK
+		rt3d::exitFatalError("Unable to create window");
+
+	context = SDL_GL_CreateContext(window); // Create opengl context and attach to window
+	SDL_GL_SetSwapInterval(1); // set swap buffers to sync with monitor's vertical refresh rate
+	return window;
+}
+
+void init(void) {
+	glEnable(GL_DEPTH_TEST);
+}
+
+void update() {
+
+}
+
+void draw(SDL_Window * window) {
+	// clear the screen
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	SDL_GL_SwapWindow(window); // swap buffers
+
+}
+
+// Program entry point - SDL manages the actual WinMain entry point for us
+int main(int argc, char *argv[]) {
+	SDL_Window * hWindow; // window handle
+	SDL_GLContext glContext; // OpenGL context handle
+	hWindow = setupRC(glContext); // Create window and render context 
+
+								  // Required on Windows *only* init GLEW to access OpenGL beyond 1.1
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) { // glewInit failed, something is seriously wrong
+		std::cout << "glewInit failed, aborting." << endl;
+		exit(1);
+	}
+	cout << glGetString(GL_VERSION) << endl;
+
+	init();
+
+	bool running = true; // set running to true
+	SDL_Event sdlEvent;  // variable to detect SDL events
+	while (running) {	// the event loop
+		while (SDL_PollEvent(&sdlEvent)) {
+			if (sdlEvent.type == SDL_QUIT)
+				running = false;
+		}
+		//update();
+		draw(hWindow); // call the draw function
+	}
+
+	SDL_GL_DeleteContext(glContext);
+	SDL_DestroyWindow(hWindow);
+	SDL_Quit();
+	return 0;
+}
